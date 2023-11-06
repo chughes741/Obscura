ae8d1eb7ff707f0823eff6795044b7a898c6ef9d
Cole <95713039+chughes741@users.noreply.github.com>
2023-11-06 15:09:21 -0500

    Added template project files

---
+++ Source/Obscura/Obscura.cpp
@@ -0,0 +1,9 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#include "Obscura.h"
+#include "Modules/ModuleManager.h"
+
+IMPLEMENT_PRIMARY_GAME_MODULE( FDefaultGameModuleImpl, Obscura, "Obscura" );
+
+DEFINE_LOG_CATEGORY(LogObscura)
+ 
